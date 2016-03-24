891c704dedfcfb79d5a3f2c4a1a90e8b3ea0f93d
Bluette1 <marylene.sawyer@gmail.com>
2016-03-24 10:42:43 +0200

    Adding my changes...

--- most_profitable_category.js
+++ most_profitable_category.js
@@ -6,44 +6,42 @@ for(var category in productsCategoryMap){
   var item = productsCategoryMap[category];
 
   for(i=0; i < item.length; i++){
-    console.log(item[i]);
+    //console.log(item[i]);
 
  if(productsPriceMap.hasOwnProperty(item[i])){
-console.log(productsPriceMap[item[i]]);
+//console.log(productsPriceMap[item[i]]);
         var priceArray =productsPriceMap[item[i]].split('R');
         var price =priceArray[1];
-        console.log(price);
-//         for(var shop in productsCostMap){
-//
-//
-//           if(productsCostMap[shop].hasOwnProperty(item[i])){
-//             // console.log(shop);
-//             // console.log(productsCostMap[shop]);
-//             // console.log(item[i]);
-//             // console.log(productsCostMap[shop][item[i]]);
-//
-//             var costArray = productsCostMap[shop][item[i]].split('R');
-// //console.log(costArray);
-//             var characters = costArray[1].split("");
-//             for(i=0;i < characters.length;i++){
-//               if(characters[i] === ','){
-//                 characters[i] = '.';
-//               }
-//
-//             }
-//             costArray[1]=characters.join("");
-//             // console.log(costArray[1]);
-//             // console.log(isNaN(costArray[1]));
-//             var cost = costArray[1];
-//
-//             var profit = Number(price)-Number(cost);
-//             totalProfit += profit;
-//             //console.log(profit);
-//
-//
-//
-//         }
-//       }
+        //console.log(price);
+        for(var shop in productsCostMap){
+
+
+          if(productsCostMap[shop].hasOwnProperty(item[i])){
+            console.log("Yeah: " + shop + ": " + item[i]);
+
+
+            var costArray = productsCostMap[shop][item[i]].split('R');
+
+            var characters = costArray[1].split("");
+            for(j=0;j < characters.length;j++){
+              if(characters[i] === ','){
+                characters[i] = '.';
+              }
+
+            }
+            costArray[1]=characters.join("");
+
+
+            var cost = costArray[1];
+
+            var profit = Number(price)-Number(cost);
+            totalProfit += profit;
+            console.log(totalProfit);
+
+
+
+        }
+      }
 //
  }
 }
@@ -55,6 +53,7 @@ if(totalProfit > maxProfit){
   maxProfit = totalProfit;
   mostProfitableCategory = category;
 }
+console.log("finished " + category);
 
 }
 return mostProfitableCategory + ": " + maxProfit;
