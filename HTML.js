b19fa22b34478fbbb655deaf0cd7a85d96ee782b
Bluette1 <marylene.sawyer@gmail.com>
2016-04-04 10:04:37 +0200

    Adding some changes...

--- HTML.js
+++ HTML.js
@@ -2,22 +2,25 @@ module.exports = function() {
 
   var Handlebars = require('handlebars');
 
-  var source = "<h1>Nelisa's Weekly Sales Report<br><h2>{{Week}}<h3><br>Most popular product:{{MostPopular}}<br>Least popular product: {{LeastPopular}} <br>Most popular category: {{MostPopularCategory}} <br> Least popular category: {{LeastPopularCategory}} <br>Most profitable product: {{MostProfitableProduct}} <br>Most profitable category: {{MostProfitableCategory}}<br> Most profitable category(If Nelisa buys from cheapest supplier): {{MostProfitableCateg}}";
+  var source = "<h1>Nelisa's Weekly Sales Report<br><h2>{{Week}}<h3><br>{{#data}}<li>The {{section}} is {{result}}</li>{{/data}}";
   var template = Handlebars.compile(source);
   var objectToString = require('./objectToString');
 
-  this.createHTML = function(string, object1, object2, object3, object4, object5, object6, object7, object8) {
+  this.createHTML = function(string, object1, object2, object3, object4, object5, object6, object7) {
     var context = {
-      Week: string,
-      MostPopular: objectToString(object1),
-      LeastPopular: objectToString(object2),
-      MostPopularCategory: objectToString(object3),
-      LeastPopularCategory: objectToString(object4),
-      MostProfitableProduct: objectToString(object5),
-      MostProfitableCategory: objectToString(object6),
-      MostProfitableCateg: objectToString(object7)
+       Week: string,
+       data: [{section:"most popular product",result:objectToString(object1)},
+       {section:"least popular product",result:objectToString(object2)},
+       {section:"most popular category",result:objectToString(object3)},
+       {section:"least popular category",result:objectToString(object4)},
+       {section:"most profitable product",result:objectToString(object5)},
+       {section:"most profitable category",result:objectToString(object6)},
+       {section:"most profitable category (if Nelisa buys from the cheapest buyer)",result:objectToString(object7)}
+     ]
+      
     }
     var html = template(context);
+
     return html;
   }
 
