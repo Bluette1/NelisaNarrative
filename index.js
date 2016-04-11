7297446be073f677cfc5ed2b45a12204b4129ceb
Bluette1 <marylene.sawyer@gmail.com>
2016-04-11 14:22:32 +0200

    Adding my changes...

--- index.js
+++ index.js
@@ -43,24 +43,24 @@ var getContext = function(string, object1, object2, object3, object4, object5, o
   return context;
 }
 
-app.get('/sales/:week_name', function(req, res) {
+app.get('/sales/week1', function(req, res) {
   var Records = require('./records');
   var records = new Records();
 
 
 
-  if (req.params.week_name === 'week1') {
+  //if (req.params.week_name === 'week1') {
     res.render('report', getContext('Week 1', records.getMostPopular(1), records.getLeastPopular(1), records.getMostPopularCategory(1), records.getLeastPopularCategory(1), records.getMostProfitable(1), records.getMostProfitableCategory(1, 1), records.getMostProfitableCategory(2, 1)));
 
-  } else if (req.params.week_name === 'week2') {
-    res.render('report', getContext('Week 2', records.getMostPopular(2), records.getLeastPopular(2), records.getMostPopularCategory(2), records.getLeastPopularCategory(2), records.getMostProfitable(2), records.getMostProfitableCategory(1, 2), records.getMostProfitableCategory(2, 2)));
-  } else if (req.params.week_name === 'week3') {
-    res.render('report', getContext('Week 3', records.getMostPopular(3), records.getLeastPopular(3), records.getMostPopularCategory(3), records.getLeastPopularCategory(3), records.getMostProfitable(3), records.getMostProfitableCategory(1, 3), records.getMostProfitableCategory(2, 3)));
-  } else if (req.params.week_name === 'week4') {
-    res.render('report', getContext('Week 4', records.getMostPopular(4), records.getLeastPopular(4), records.getMostPopularCategory(4), records.getLeastPopularCategory(4), records.getMostProfitable(4), records.getMostProfitableCategory(1, 4), records.getMostProfitableCategory(2, 4)));
-  } else if (req.params.week_name === 'month') {
-    res.render('report', getContext('Overall: Report for the month', records.getMostPopular(5), records.getLeastPopular(5), records.getMostPopularCategory(5), records.getLeastPopularCategory(5), records.getMostProfitable(5), records.getMostProfitableCategory(1, 5), records.getMostProfitableCategory(2, 5)));
-  }
+  // } else if (req.params.week_name === 'week2') {
+  //   res.render('report', getContext('Week 2', records.getMostPopular(2), records.getLeastPopular(2), records.getMostPopularCategory(2), records.getLeastPopularCategory(2), records.getMostProfitable(2), records.getMostProfitableCategory(1, 2), records.getMostProfitableCategory(2, 2)));
+  // } else if (req.params.week_name === 'week3') {
+  //   res.render('report', getContext('Week 3', records.getMostPopular(3), records.getLeastPopular(3), records.getMostPopularCategory(3), records.getLeastPopularCategory(3), records.getMostProfitable(3), records.getMostProfitableCategory(1, 3), records.getMostProfitableCategory(2, 3)));
+  // } else if (req.params.week_name === 'week4') {
+  //   res.render('report', getContext('Week 4', records.getMostPopular(4), records.getLeastPopular(4), records.getMostPopularCategory(4), records.getLeastPopularCategory(4), records.getMostProfitable(4), records.getMostProfitableCategory(1, 4), records.getMostProfitableCategory(2, 4)));
+  // } else if (req.params.week_name === 'month') {
+  //   res.render('report', getContext('Overall: Report for the month', records.getMostPopular(5), records.getLeastPopular(5), records.getMostPopularCategory(5), records.getLeastPopularCategory(5), records.getMostProfitable(5), records.getMostProfitableCategory(1, 5), records.getMostProfitableCategory(2, 5)));
+  // }
 
 
 });
