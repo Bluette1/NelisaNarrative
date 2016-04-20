db7956127355e348615d4d1d6b7ac2ef8b245949
Bluette1 <marylene.sawyer@gmail.com>
2016-04-20 15:23:02 +0200

    Adding my changes...

--- js/data.js
+++ js/data.js
@@ -1,5 +1,16 @@
 var mysql = require('mysql');
 
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    // your connection details here
+         host: 'localhost',
+         user: 'bluette',
+         password: 'password',
+         database: 'nelisa'
+});
+
 var readTheFile = require('./readTheFile');
 var file = readTheFile('../data/categories.csv');
 var products_category_map = require('./products_category_map');
@@ -13,18 +24,9 @@ for (category in map){
 }
 
 //console.log(category_array);
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    // your connection details here
-         host: 'localhost',
-         user: 'bluette',
-         password: 'password',
-         database: 'nelisa'
-});
+var sql = "INSERT INTO categories (description) VALUES ?";
 
-// var sql = "INSERT INTO categories (description) VALUES ?";
-//
-//
+
 // connection.query(sql, [category_array], function(err) {
 //     if (err) throw err;
 //     connection.end();
@@ -35,7 +37,8 @@ var readTheFile = require('./readTheFile');
 var file1 = readTheFile('../data/week1.csv'),
     file2 = readTheFile('../data/week2.csv'),
     file3 = readTheFile('../data/week3.csv'),
-    file4 = readTheFile('../data/week4.csv');
+    file4 = readTheFile('../data/week4.csv'),
+    categories = readTheFile('../data/categories.csv');
 var products_price_map_overall =  require('./products_price_map_overall');
 var products_price_map = products_price_map_overall(file1,file2,file3,file4);
 var products_array = [];
@@ -43,16 +46,32 @@ for (var product in products_price_map){
   var a = [];
   a.push(product);
   a.push(products_price_map[product]);
-  products_array.push(a);
+
+for(i=0;i < categories.length;i++){
+ var factors = categories[i].split(",");
+ if (product === factors[0]){
+   a.push(factors[1]);
+ }
+
+
+
 }
-//console.log(products_array);
-//
-// var sql1 = "INSERT INTO products (description, price) VALUES ?";
+
+
+products_array.push(a);
+
+}
+console.log(products_array);
+
+
+var sql1 = "INSERT INTO products (description, price,category) VALUES ?";
 //
 // connection.query(sql1, [products_array], function(err) {
 //     if (err) throw err;
 //     connection.end();
 // });
+
+
 var products_quantity_map = require('./products_quantity_map_overall');
 
 var array = products_quantity_map(file1,file2,file3,file4);
@@ -86,14 +105,15 @@ for(i = 0; i < array.length; i++){
 
 
 
-// console.log(sales_array);
+//console.log(sales_array);
 
-// var sql2 = "INSERT INTO sales (description, quantity, week) VALUES ?";
+var sql2 = "INSERT INTO sales (description, quantity, week) VALUES ?";
 //
 // connection.query(sql2, [sales_array], function(err) {
 //     if (err) throw err;
 //     connection.end();
 // });
+
 var products_cost_map = require('./products_cost_map1');
 var purchases = readTheFile('../data/purchases.csv');
 var arr =  products_cost_map(purchases);
@@ -109,11 +129,11 @@ for(i=0; i < arr.length; i++){
 purchases_array.push(a);
 }
 
-// console.log(purchases_array);
+//console.log(purchases_array);
+
+var sql3 = "INSERT INTO purchases (description, quantity, cost, total_cost, shop) VALUES ?";
 //
-// var sql3 = "INSERT INTO purchases (description, quantity, cost, total_cost, shop) VALUES ?";
-//
-// connection.query(sql3, [purchases_array], function(err) {
-//     if (err) throw err;
-//     connection.end();
-// });
+connection.query(sql3, [purchases_array], function(err) {
+    if (err) throw err;
+    connection.end();
+});
--- script.sql
+++ script.sql
@@ -26,12 +26,12 @@ use nelisa;
 --     product_id int,
 --     foreign key (product_id) references products(id)
 -- );
- --
- -- ALTER TABLE sales DROP COLUMN price ;
- -- ALTER TABLE sales ADD quantity decimal(10);
- -- ALTER TABLE sales ADD week int;
-
-
+--
+--  ALTER TABLE sales DROP COLUMN price ;
+--  ALTER TABLE sales ADD quantity decimal(10);
+--  ALTER TABLE sales ADD week int;
+--
+--
 -- create table purchases (
 --     id int primary key auto_increment,
 --         description char(100) not null,
@@ -39,9 +39,26 @@ use nelisa;
 --     product_id int,
 --     foreign key (product_id) references products(id)
 -- );
-
+--
 -- ALTER TABLE purchases DROP COLUMN price;
 -- ALTER TABLE purchases ADD quantity int;
 -- ALTER TABLE purchases ADD cost decimal(10,2);
 -- ALTER TABLE purchases ADD total_cost decimal(10,2);
-ALTER TABLE purchases ADD shop char(100);
+-- ALTER TABLE purchases ADD shop char(100);
+-- ALTER TABLE products ADD category char(100);
+
+-- UPDATE products AS p
+-- INNER JOIN categories AS c
+-- ON p.category = c.description
+-- SET p.category_id = c.id;
+
+
+-- UPDATE purchases AS p
+-- INNER JOIN products AS pr
+-- ON p.description = pr.description
+-- SET p.product_id = pr.id;
+
+UPDATE sales AS s
+INNER JOIN products AS pr
+ON s.description = pr.description
+SET s.product_id = pr.id;
